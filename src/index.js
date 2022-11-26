4f33e6631505ee2292100a9ef2bb4fae3c90d8cf
simeonxs <119071613+simeonxs@users.noreply.github.com>
2022-11-26 16:31:49 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,27 @@ window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
   const app = new Application();
   // Used to access the app instance by the automated tests
-  app.setEmojis(["🐒", "🦍", "🦧"]);
+  
+  let result = addBananas();
+  function setEmojis(emojis) {
+    let wrapper = document.querySelector('.emojis');
+    wrapper.innerHTML = "";
+    let p = document.createElement('p');
+    p.innerText = emojis;
+    document.wrapper.appendChild(p);
+  }
+  
+  app.setEmojis(result);
   app.addBananas();
+  
+  function addBananas() {
+    let arr = ["🐒", "🦍", "🦧"];
+    arr.map(el => {
+      el += "🍌"
+    })
+    return arr
+  }
+
+  
   window.__JS_APP = app;
 });
